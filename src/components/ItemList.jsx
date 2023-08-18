f6e7448b20bb57345279ab26ec56874d3ab7bc29
feli <felitaurel@gmail.com>
2023-08-18 06:04:41 -0300

    grid en itemlist

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,21 +3,32 @@ import ActionAreaCard from './ItemCard'
 import { Grid } from '@mui/material'
 
 function ItemList(props) {
-  return (<div className = 'flex-container' sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, alignItems: 'stretch'}}>
+  const items = props.products.map(item =>
+    <ActionAreaCard Key={item.id} {...item}/>)/* {props.products.map((item) => (
+        
+        
+      //probablemente el problema del display es aca, que estoy haciendo un loop y mostrando un solo componente a la vez, deberia quizas usar un grid donde cargue todos los componentes y mostrarlos juntos 
+        <ActionAreaCard Key={item.id} {...item}></ActionAreaCard>
+      
+    )
+  )}
+ */
+  return (
       
     
-    Listado de Productos
     
-      {props.products.map((item) => (
-        
-        
-          //probablemente el problema del display es aca, que estoy haciendo un loop y mostrando un solo componente a la vez, deberia quizas usar un grid donde cargue todos los componentes y mostrarlos juntos 
-            <ActionAreaCard Key={item.id} {...item}></ActionAreaCard>
-          
-        )
-      )}
     
-  </div>
+     
+    <Grid container justifyItems="center">
+       {props.products.map((item) => (
+        
+        <Grid item xs={4}>
+          <ActionAreaCard Key={item.id} {...item}></ActionAreaCard>
+          </Grid>
+      )
+    )}
+    </Grid>
+  
   )
 }
 

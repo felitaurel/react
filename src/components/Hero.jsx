e8765dd2b773683149382a6f45462887cf67b0e3
feli <ftaurel@fi.uba.ar>
2024-07-19 01:23:55 -0300

    saque contenid innecesario del banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,15 +18,7 @@ const HeroBanner = () => {
         padding: '20px'
       }}
     >
-      <Typography variant="h2" component="h1" gutterBottom>
-        Welcome to Our Store
-      </Typography>
-      <Typography variant="h5" component="p" gutterBottom>
-        Discover the Best Deals on Your Favorite Products
-      </Typography>
-      <Button variant="contained" color="primary" size="large">
-        Shop Now
-      </Button>
+    
     </Box>
   );
 }

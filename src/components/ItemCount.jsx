078b4a12e144e7f48405bcdded9d8e43b8ea8785
feli <ftaurel@fi.uba.ar>
2024-07-24 00:36:06 -0300

    cambio en itemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,46 +3,10 @@ import React, { useState } from "react";
 function ItemCount(props) {
     const [clickCount, setClickCount] = useState(1);
   
-    function handleClickAdd() {
-      if (clickCount === props.stock) {
-      } else {
-        setClickCount(clickCount + 1);
-      }
-    }
-  
-    function handleClickSub() {
-      if (clickCount > 1) {
-        setClickCount(clickCount - 1);
-      }
-    }
+   
   
     return (
       <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          
-        >
-          <button
-            disabled={props.stock === 0}
-            
-            onClick={handleClickSub}
-          >
-            -
-          </button>
-          <h2 >{clickCount}</h2>
-          <button
-            disabled={props.stock === 0}
-          
-            onClick={handleClickAdd}
-          >
-            +
-          </button>
-        </div>
   
         <button
           disabled={props.stock === 0}

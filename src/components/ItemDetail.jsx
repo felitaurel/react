e08dce0ae2eaf026b6bf64ee205a70cdf153f0fb
feli <ftaurel@fi.uba.ar>
2024-07-24 00:17:56 -0300

    cambios en itemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -13,15 +13,15 @@ function ItemDetail(props) {
   const { addToCart, getItemInCart } = useContext(cartContext);
   const {id, nombre, descripcion, img, precio, category, stock} = props;
   const itemInCart = getItemInCart(id)
-  console.log(id)
-  const [isAddedToCart, setIsAddedToCart] = useState(false);
-  const maxItems = itemInCart
-    ? stock - itemInCart.count
-    : stock;
   
-  function handleAddToCart(clickCount) {
-    addToCart(props, clickCount);
-    alert(`${nombre} agregado al carrito, cantidad: ${clickCount}`);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
+  
+  
+  const tallesPosibles = [36,37,38,39,40,41,42,43,44]
+
+  function handleAddToCart() {
+    addToCart(props);
+    alert(`${nombre} agregado al carrito`);
     setIsAddedToCart(true);
   }
   return(
@@ -40,13 +40,13 @@ function ItemDetail(props) {
       
     
     {itemInCart && (
-      <h2>Ya agregaste {itemInCart.count} unidades de este producto</h2>
+      <h2>Ya agregaste {itemInCart} al carrito</h2>
     )}
     {stock > 0 ? (
       isAddedToCart ? (
         <Link to="/cart" style={{color: '#000000'}}>Ir al carrito</Link> //cambiarle el color al link porque no se ve sobre el celeste
       ) : (
-        <ItemCount stock={maxItems} onConfirm={handleAddToCart} />
+        <Button onClick = {handleAddToCart}> Agregar al carrito </Button>
       )
     ) : (
       <p>No hay stock disponible</p>
